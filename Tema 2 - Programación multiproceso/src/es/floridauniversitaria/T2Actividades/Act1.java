ae6439996768c78ed5ad58265994b8dfa1687d5c
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-10-07 18:05:48 +0200

    mutreadinfg hecho

--- Tema 2 - Programación multiproceso/src/es/floridauniversitaria/T2Actividades/Act1.java
+++ Tema 2 - Programación multiproceso/src/es/floridauniversitaria/T2Actividades/Act1.java
@@ -1,22 +1,30 @@
 package es.floridauniversitaria.T2Actividades;
 
-public class Act1 {
+class Act1 {
 
-
-		static int sumador() {
-		int num1 = 2;
-		int num2 = 10;
+	int sumador(int num1, int num2) {
 		int sum = 0;
 		
-		for (int i = num1; i < num2; i++) {
-			
-			sum+= i;
-			
+		
+		System.out.println("Sumando de " + num1 + " hasta " + num2);
+		for (int i = num1; i <= num2; i++) {
+
+			sum += i;
+
 		}
-		
+
 		return sum;
-		
-		
+
+	}
+
+	public static void main(String[] args) {
+
+		Act1 act1 = new Act1();
+
+		int sum = act1.sumador(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+
+		System.out.println(sum);
+
 	}
 
 }
--- Tema 2 - Programación multiproceso/src/es/floridauniversitaria/T2Actividades/Act2.java
+++ Tema 2 - Programación multiproceso/src/es/floridauniversitaria/T2Actividades/Act2.java
@@ -7,40 +7,45 @@ import java.util.List;
 
 public class Act2 {
 
-	static void execute(String dir) {
+	static void execute(int num1, int num2) {
+
+		String clase = "es.floridauniversitaria.T2Actividades.Act1";
+		File dir = new File ("C:\\Users\\alvar\\Desktop\\EclipsePSP\\Tema 2 - Programación multiproceso\\guardados");
+		File save = new File ("Save.txt");
 		
-			
-					File direct = new File(dir);
-			
-					String clase = "es.floridauniversitaria.T2Actividades";
-					String javaHome = System.getProperty("java.home");
-					String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
-					String classpath = System.getProperty("java.class.path");
-					String className = clase;
-					List<String> command = new ArrayList<>();
-					command.add(javaBin);
-					command.add("-cp");
-					command.add(classpath);
-					command.add(className);
+		try {
+			String javaHome = System.getProperty("java.home");
+			String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+			String classpath = System.getProperty("java.class.path");
+			String className = clase;
 
-					ProcessBuilder builder = new ProcessBuilder(command);
-					builder.directory(direct);
+			List<String> command = new ArrayList<>();
+			command.add(javaBin);
+			command.add("-cp");
+			command.add(classpath);
+			command.add(className);
+			command.add(Integer.toString(num1));
+			command.add(Integer.toString(num2));
+			
+			ProcessBuilder builder = new ProcessBuilder(command);
+			
+			
+			builder.directory(dir);
+			builder.redirectOutput(save).start();
 
-					try {
-						Process p = builder.start();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main(String[] args) {
-
-		//Act2.execute("./src/es/floridauniversitaria/T2Actividades/Act1.java");
 		
-		Act2.execute("./Act1.java");
-
-		System.out.println("Exit");
+		
+		Act2.execute(2,10);
+	
 
 	}
 
