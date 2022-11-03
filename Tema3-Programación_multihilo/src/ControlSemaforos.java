bb83cf141e01577dedcc04f15f993bbf628ae491
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-11-03 22:31:37 +0100

    repasin

--- Tema3-Programación_multihilo/src/ControlSemaforos.java
+++ Tema3-Programación_multihilo/src/ControlSemaforos.java
@@ -2,8 +2,6 @@
 public class ControlSemaforos {
 
 	String semaforo;
-	
-	
 
 	public ControlSemaforos(String semaforo) {
 		super();
@@ -11,15 +9,70 @@ public class ControlSemaforos {
 	}
 
 	public void encenderSemaforo1() {
-		
+
+		while (true) {
+			
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			synchronized (this) {
+				
+				if (this.semaforo.equals("verde")) {
+
+					this.semaforo = "rojo";
+				} else
+					this.semaforo = "verde";
+
+				notify();
+
+					System.out.println("Semaforo1: "+this.semaforo);
+					
+					try {
+						wait();
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+			}
+		}
 	}
 
 	public void encenderSemaforo2() {
+		while (true) {
+			
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			synchronized (this) {
+				
+				if (this.semaforo.equals("verde")) {
+
+					this.semaforo = "rojo";
+				} else
+					this.semaforo = "verde";
+
+				notify();
+
+					System.out.println("Semaforo2: "+this.semaforo);
+					
+					try {
+						wait();
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+			}
+		}
+
 	}
 
-		
-		
-		
 }
-
-
