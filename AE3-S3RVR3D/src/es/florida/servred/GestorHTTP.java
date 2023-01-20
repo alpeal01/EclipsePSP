aa04ab28c3df10b2aaecd822cae913350a207ff5
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2023-01-20 17:25:10 +0100

    lol

--- AE3-S3RVR3D/src/es/florida/servred/GestorHTTP.java
+++ AE3-S3RVR3D/src/es/florida/servred/GestorHTTP.java
@@ -1,22 +1,43 @@
 package es.florida.servred;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
 import com.sun.net.httpserver.*;
 
-
 public class GestorHTTP implements HttpHandler {
+
+	private String handleGetRequest(HttpExchange httpExchange) {
+		
+		System.out.println(httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1]);
+		
+		return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
+	}
+	
+	private void handleGETResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
+		OutputStream outputStream = httpExchange.getResponseBody();
+		String htmlResponse = "<html><body>Hola mundo:"+requestParamValue+"</body></html>";
+		httpExchange.sendResponseHeaders(200, htmlResponse.length());
+		outputStream.write(htmlResponse.getBytes());
+		outputStream.flush();
+		outputStream.close();
+		}
+
 	@Override
 	public void handle(HttpExchange httpExchange) throws IOException {
 		
-		String requestParamValue = null;
+		System.out.println("handle");
 		
+		String requestParamValue = null;
+
 		if ("GET".equals(httpExchange.getRequestMethod())) {
+
+			requestParamValue = handleGetRequest(httpExchange);
 			
-//			requestParamValue = handleGetRequest(httpExchange);
-//			handleGETResponse(httpExchange, requestParamValue);
-			
+			handleGETResponse(httpExchange, requestParamValue);
+
 		} else if ("POST".equals(httpExchange.getRequestMethod())) {
-			
+
 //			requestParamValue = handlePostRequest(httpExchange);
 //			
 //			handlePOSTResponse(httpExchange, requestParamValue);
