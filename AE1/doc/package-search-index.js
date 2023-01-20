7fd78e103c75ebb72e018b03131785cf06725fcd
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2023-01-20 15:59:09 +0100

    all

--- AE1/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"es.florida.ae1"}];updateSearchResults();
--- AE1/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.florida.ae1","l":"Lanzadora"},{"p":"es.florida.ae1","l":"Procesadora"}];updateSearchResults();
